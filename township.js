2936a4533d13aa1036cfb849d71e48580d3e513b
cyhsiao2239 <mymymygod2000@gmail.com>
2025-05-09 15:23:45 +0800

    修改jsaon頭尾及js連接的部分，感謝科技進步

--- township.js
+++ township.js
@@ -1,36 +1,67 @@
-
 fetch('題目.json')
     .then(response => response.json())
     .then(data => {
-        // 題目一：雲嘉南
-        const yunJiaNanCities = ['雲林縣', '嘉義縣', '臺南市'];
+        // 題目一：雲嘉南區域有幾個縣市？
+        const yunJiaNanCityNames = ['雲林縣', '嘉義縣', '臺南市'];
         const southCityAll = [];
 
-        yunJiaNanCities.forEach(city => {
-            if (data[city]) {
-                southCityAll.push(city);
-            }
+        data.forEach(area => {
+            area.cities.forEach(city => {
+                if (yunJiaNanCityNames.includes(city.cityName)) {
+                    southCityAll.push(city.cityName);
+                }
+            });
         });
 
         document.querySelector('.south-city-num').textContent = southCityAll.length;
         document.querySelector('.south-city-all').textContent = southCityAll.join('、');
 
-        // 題目二：新北市鄉鎮
-        const newTaipeiDistricts = data['新北市'] || [];
-        document.querySelector('.north-city-num').textContent = newTaipeiDistricts.length;
-        document.querySelector('.north-city-all').textContent = newTaipeiDistricts.join('、');
+        // 題目二：新北市有幾個鄉鎮？
+        let newTaipeiTownships = [];
 
-        // 題目三：南投市
-        const nantouCode = data['南投縣']?.['南投市'];
-        document.querySelector('.nantou-townshipID').textContent += nantouCode;
-        document.querySelector('.nantou-townshipName').textContent += '南投市';
+        data.forEach(area => {
+            area.cities.forEach(city => {
+                if (city.cityName === '新北市') {
+                    newTaipeiTownships = city.townships;
+                }
+            });
+        });
 
-        // 題目四：太麻里鄉
-        const taimaliCode = data['臺東縣']?.['太麻里鄉'];
-        document.querySelector('.taimali-townshipID').textContent += taimaliCode;
-        document.querySelector('.taimali-townshipName').textContent += '太麻里鄉';
+        document.querySelector('.north-city-num').textContent = newTaipeiTownships.length;
+        document.querySelector('.north-city-all').textContent = newTaipeiTownships.map(t => t.townshipName).join('、');
+
+        // 題目三：南投市的郵遞區號
+        let nantouTownship = null;
+
+        data.forEach(area => {
+            area.cities.forEach(city => {
+                if (city.cityName === '南投縣') {
+                    nantouTownship = city.townships.find(t => t.townshipName === '南投市');
+                }
+            });
+        });
+
+        if (nantouTownship) {
+            document.querySelector('.nantou-townshipID').textContent += nantouTownship.townshipId;
+            document.querySelector('.nantou-townshipName').textContent += nantouTownship.townshipName;
+        }
+
+        // 題目四：太麻里鄉的郵遞區號
+        let taimaliTownship = null;
+
+        data.forEach(area => {
+            area.cities.forEach(city => {
+                if (city.cityName === '臺東縣') {
+                    taimaliTownship = city.townships.find(t => t.townshipName === '太麻里鄉');
+                }
+            });
+        });
+
+        if (taimaliTownship) {
+            document.querySelector('.taimali-townshipID').textContent += taimaliTownship.townshipId;
+            document.querySelector('.taimali-townshipName').textContent += taimaliTownship.townshipName;
+        }
     })
     .catch(error => {
         console.error('載入 JSON 錯誤:', error);
     });
-
